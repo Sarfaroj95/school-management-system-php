2fe4773cdb0dd21b45c9414bf9db798cad823365
sarfaroj <sarfaroj95@gmail.com>
2026-09-13 22:36:08 +0530

    chore: update database connection credentials and add development archive

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-  $conn=mysqli_connect("localhost","root","","school")or die("Failed to connect! pls check connection  then try again");
+  $conn=mysqli_connect("sql209.infinityfree.com","if0_42906106","kgcKhMzTT8UNe6K","if0_42906106_old_school_db")or die("Failed to connect! pls check connection  then try again");
 ?>
